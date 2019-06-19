1215ee970e4828d9da428b9be579e23b0bdc137f
Rafael Camelo <jayrorrc@gmail.com>
2019-06-19 00:08:29 -0300

    Add data format string

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -12,6 +12,7 @@ namespace MvcMovie.Models
         [Required]
         public string Title { get; set; }
 
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
